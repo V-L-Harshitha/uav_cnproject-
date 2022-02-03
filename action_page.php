a6e601eb6f577aadd674a8f757c2a8229503d4a5
V-L-Harshitha <66424026+V-L-Harshitha@users.noreply.github.com>
2022-02-03 22:33:08 +0530

    Delete action_page.php

--- action_page.php
+++
@@ -1,9 +0,0 @@
-<?php
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["uname"]["name"]);
-$uploadOk = 1;
-if(isset($_POST["submit"])) {
-  $check = getimagesize($_FILES["uname"]["tmp_name"]);
-  
-}
-?>
